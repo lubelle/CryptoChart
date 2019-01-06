863bd795356705f9c01f694664dc3a6dec26b7bc
lubelle <belleliullh@gmail.com>
2019-01-06 11:44:21 -0500

    add routing, not-found page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CryptoService } from 'src/services/crypto.service';
 import { CryptoTableComponent } from './crypto-table/crypto-table.component';
+import { BitcoinStatsComponent } from './bitcoin-stats/bitcoin-stats.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoutes: Routes = [
+  { path: '', component: CryptoTableComponent },
+  { path: 'bitcoinStats', component: BitcoinStatsComponent },
+  { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CryptoTableComponent
+    CryptoTableComponent,
+    BitcoinStatsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [CryptoService],
   bootstrap: [AppComponent]
