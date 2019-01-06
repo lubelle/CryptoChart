4502fdc4ca6f5bff92b0ef3a91f6ca44204a89cb
lubelle <belleliullh@gmail.com>
2019-01-06 11:09:59 -0500

    add sorting by column

--- src/app/crypto-table/crypto-table.component.ts
+++ src/app/crypto-table/crypto-table.component.ts
@@ -9,7 +9,10 @@ import { CryptoCurrency } from 'src/models/crypto-currency.class';
     styleUrls: ['./crypto-table.component.css']
 })
 export class CryptoTableComponent {
+    isToggle = false;
     public top100Cryptos: CryptoCurrency[];
+    public sortValues: any = { rank: false, market_cap_usd: true, available_supply: false,
+        percent_change_24h: false, price_usd: false, name: false};
     constructor(public cryptoService: CryptoService) {
         this.getTop100Cryptos();
     }
@@ -21,5 +24,41 @@ export class CryptoTableComponent {
             });
             console.log(this.top100Cryptos);
         });
+    }
+    sortString(sortValue: boolean, key?: string): void {
+        if (sortValue) {
+            this.top100Cryptos = this.top100Cryptos.sort((a, b) => {
+                const nameA = a.name.toUpperCase();
+                const nameB = b.name.toUpperCase();
+                if (nameA < nameB) {
+                    return -1;
+                } else if (nameA > nameB) {
+                    return 1;
+                }
+                return 0;
+            });
+        } else {
+            this.top100Cryptos = this.top100Cryptos.sort((a, b) => {
+                const nameA = a.name.toUpperCase();
+                const nameB = b.name.toUpperCase();
+                if (nameA > nameB) {
+                    return -1;
+                } else if (nameA < nameB) {
+                    return 1;
+                }
+                return 0;
+            });
+        }
+    }
+    public sortNumeric(sortValue: string, key: string) {
+        if (sortValue) {
+            this.top100Cryptos = this.top100Cryptos.sort((a: CryptoCurrency, b: CryptoCurrency) => {
+                return a[key] - b[key];
+            });
+        } else {
+            this.top100Cryptos = this.top100Cryptos.sort((a: CryptoCurrency, b: CryptoCurrency) => {
+                return b[key] - a[key];
+            });
+        }
     }
 }
