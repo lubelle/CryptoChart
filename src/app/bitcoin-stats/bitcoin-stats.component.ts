89149ceb8b09ca875df5d18f7cf896517a280304
lubelle <belleliullh@gmail.com>
2019-01-07 09:58:34 -0500

    price conversion; set up prices array for chart (coordinate y)

--- src/app/bitcoin-stats/bitcoin-stats.component.ts
+++ src/app/bitcoin-stats/bitcoin-stats.component.ts
@@ -1,3 +1,4 @@
+import { PriceCoordinates } from './../../models/price-coordinates.interface';
 import { Component, OnInit } from '@angular/core';
 import { CryptoService } from 'src/services/crypto.service';
 import { BitcoinPrice } from 'src/models/bitcoin-price.class';
@@ -9,6 +10,8 @@ import { BitcoinPrice } from 'src/models/bitcoin-price.class';
 })
 export class BitcoinStatsComponent implements OnInit {
   bitcoinStats: BitcoinPrice = new BitcoinPrice();
+  prices: number[];
+
   constructor(public cryptoService: CryptoService) { }
 
   ngOnInit() {
@@ -18,7 +21,17 @@ export class BitcoinStatsComponent implements OnInit {
   public getYearlyBitcoinPrice() {
     this.cryptoService.getBitcoinPriceStats().subscribe((data: any) => {
       this.bitcoinStats = new BitcoinPrice(data);
+      this.prices = this.convertPrices();
+      console.log(this.prices);
     });
   }
 
+  public convertPrices(): number[] {
+    // filter out all the values that are in our values right for prices and 
+    // put them into an array of prices
+    const prices = this.bitcoinStats.values.map((coordinates: PriceCoordinates) => {
+      return Number((coordinates.y).toFixed(2));
+    });
+    return prices;
+  }
 }
