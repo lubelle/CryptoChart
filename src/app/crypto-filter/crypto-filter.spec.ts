b74bd575e86cd608ba71d04b32d92866c62b678d
lubelle <belleliullh@gmail.com>
2019-01-09 15:42:24 -0500

    unit tests on filter: return all cryptos, return positive growth cryptos, return negative growth cryptos

--- src/app/crypto-filter/crypto-filter.spec.ts
+++ src/app/crypto-filter/crypto-filter.spec.ts
@@ -57,6 +57,59 @@ describe('CryptoFilterComponent', () => {
       expect(Array.isArray(app.filteredCryptos)).toBe(true);
       expect(app.filteredCryptos.length).toBe(25);
 
+    }));
+
+    it('retruns all cryptos', async(() => {
+      // arrange
+      const fixture = TestBed.createComponent(CryptoFilterComponent);
+      const app = fixture.debugElement.componentInstance;
+      app.cryptos = Array(100).fill(new CryptoCurrency());
+
+      // act
+      app.percentChangeFilter();
+
+      // assert
+      expect(app.filteredCryptos[0] instanceof CryptoCurrency);
+      expect(app.filteredCryptos.length).toBe(100);
+    }));
+
+    it('returns positive 24h growth cryptos', async(() => {
+      // arrange
+      const fixture = TestBed.createComponent(CryptoFilterComponent);
+      const app = fixture.debugElement.componentInstance;
+      app.cryptos = [
+        new CryptoCurrency({percent_change_24h: -1}),
+        new CryptoCurrency({percent_change_24h: 6}),
+        new CryptoCurrency({percent_change_24h: 5})
+      ];
+      app.percentChange = 'Positive';
+
+      // act
+      app.percentChangeFilter();
+
+      // assert
+      expect(app.filteredCryptos[0] instanceof CryptoCurrency);
+      expect(app.filteredCryptos.length).toBe(2);
+
+    }));
+
+    it('returns negative 24h growth cryptos', async(() => {
+      // arrange
+      const fixture = TestBed.createComponent(CryptoFilterComponent);
+      const app = fixture.debugElement.componentInstance;
+      app.cryptos = [
+        new CryptoCurrency({percent_change_24h: -1}),
+        new CryptoCurrency({percent_change_24h: 6}),
+        new CryptoCurrency({percent_change_24h: 5})
+      ];
+      app.percentChange = 'Negative';
+
+      // act
+      app.percentChangeFilter();
+
+      // assert
+      expect(app.filteredCryptos[0] instanceof CryptoCurrency);
+      expect(app.filteredCryptos.length).toBe(1);
 
     }));
 });
