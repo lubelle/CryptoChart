97bfb7b8d0859e393722eba5f3130f00330b2e7b
lubelle <belleliullh@gmail.com>
2019-01-13 21:52:11 -0500

    add unit test on service.getAllCryptos()

--- src/app/crypto-table/crypto-table.spec.ts
+++ src/app/crypto-table/crypto-table.spec.ts
@@ -1,6 +1,8 @@
+import { CryptoCurrency } from './../../models/crypto-currency.class';
 import { async, TestBed, ComponentFixture } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { from } from 'rxjs';
 
 import { CryptoTableComponent } from './crypto-table.component';
 import { CryptoService } from 'src/services/crypto.service';
@@ -8,6 +10,7 @@ import { CryptoService } from 'src/services/crypto.service';
 describe('CryptoTableComponent', () => {
     let fixture: ComponentFixture<CryptoTableComponent>;
     let component;
+    let service: CryptoService;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -18,7 +21,9 @@ describe('CryptoTableComponent', () => {
         }).compileComponents();
 
         fixture = TestBed.createComponent(CryptoTableComponent);
-        component = fixture.debugElement.componentInstance;
+        service = new CryptoService(null);
+        // component = fixture.debugElement.componentInstance;
+        component = new CryptoTableComponent(service);
     }));
 
     it('should create the component', () => {
@@ -33,6 +38,20 @@ describe('CryptoTableComponent', () => {
     it('should return supported currency BTC', () => {
         component.listenPriceUnit('BTC');
         expect(component.priceUnit).toContain('BTC');
+    });
+
+    it('should set top100Cryptos property with the data returned from the server', () => {
+        // arrange
+        const cryptos: CryptoCurrency[] = [new CryptoCurrency()];
+        spyOn(service, 'getAllCryptos').and.callFake(() => {
+            return from([cryptos]);
+        });
+
+        // act
+        component.getTop100Cryptos();
+
+        // assert
+        expect(component.top100Cryptos.length).toBe(1);
     });
 
 });
