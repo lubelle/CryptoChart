fc0bdf77a1b2b0b71d6cc7f4451ab647227a83a1
lubelle <belleliullh@gmail.com>
2019-01-13 20:05:46 -0500

    add unit test on service

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,10 +5,12 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CryptoService } from 'src/services/crypto.service';
 import { BitcoinMarket } from 'src/models';
+import { from } from 'rxjs';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let component;
+  let service: CryptoService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -25,7 +27,9 @@ describe('AppComponent', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
-    component = fixture.debugElement.componentInstance;
+    // component = fixture.debugElement.componentInstance;
+    service = new CryptoService(null);
+    component = new AppComponent(service);
 
   }));
 
@@ -36,4 +40,18 @@ describe('AppComponent', () => {
   it('instance of BitcoinMarket', async(() => {
     expect(component.bitcoinMarketCap instanceof BitcoinMarket).toBe(true);
   }));
+
+  it('should set bitcoinMarketCap property with the data returned from the server', () => {
+    // arrange
+    const bitcoinMarket = new BitcoinMarket({totol_market_cap: 0});
+    spyOn(service, 'getBitcoinmarketCap').and.callFake(() => {
+      return from([bitcoinMarket]);
+    });
+
+    // act
+    component.getBitcoinStats();
+
+    // assert
+    expect(component.bitcoinMarketCap).toBe(bitcoinMarket);
+  });
 });
